0cea89e22d97e09f6e34a323ec6ceab1866442cf
Eterfeier <dika.news321@gmail.com>
2021-12-07 14:27:57 +0700

    Addition to ForumActivity and created ForumAdapter

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # CFreelance_2nd
+%s, editorName
 %d %s %d, date, month, year
     - %s, progressDetails[0]
     - %s, progressDetails[1]
 
 -------------------------------------------------------------------------------
 
+Andika
 6 Dec 2021
     - Membuat progres di Home dan menerapkan RecyclerView di Category
     - Membuat Bottom Navigation Bar
--- app/src/main/java/com/latihan/android/cfreelance_2nd/ForumActivity.java
+++ app/src/main/java/com/latihan/android/cfreelance_2nd/ForumActivity.java
@@ -1,8 +1,14 @@
 package com.latihan.android.cfreelance_2nd;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ForumActivity extends AppCompatActivity {
 
@@ -10,5 +16,38 @@ public class ForumActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forum);
+
+
+
+
+
+
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setSelectedItemId(R.id.navCategoryMenu);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.navHomeMenu:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.navCategoryMenu:
+                        startActivity(new Intent(getApplicationContext(), CategoryActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.navForumMenu:
+                        return true;
+                    case R.id.navCartMenu:
+//                        startActivity(new Intent(getApplicationContext(), CategoryActivity.class));
+//                        overridePendingTransition(0,0);
+                        Toast.makeText(getApplicationContext(), "OnGoing Project", Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
